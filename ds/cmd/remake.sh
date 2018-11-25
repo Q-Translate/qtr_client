53b83a11f0966af6ee543605f87261da3e7f8d88
Dashamir Hoxha <dashohoxha@gmail.com>
2018-11-25 04:46:31 +0100

    Improve restore and remake

--- ds/cmd/remake.sh
+++ ds/cmd/remake.sh
@@ -1,14 +1,20 @@
 cmd_remake_help() {
     cat <<_EOF
-    remake
+    remake [full | data]
         Reconstruct again the container, preserving the existing data.
+        By default it will make a full backup of the database and application
+        files, but with option 'data' only a partial backup of the application
+        and database will be saved and restored.
 
 _EOF
 }
 
 cmd_remake() {
+    local full='full'
+    [[ $1 == 'data' ]] && full='data'
+
     # backup
-    ds backup
+    ds backup $full
 
     # reinstall
     ds remove
@@ -17,6 +23,6 @@ cmd_remake() {
     ds wsproxy ssl-cert
 
     # restore
-    local backup_file="backup-full-$(date +%Y%m%d).tgz"
+    local backup_file="backup-$full-$(date +%Y%m%d).tgz"
     ds restore $backup_file
 }
